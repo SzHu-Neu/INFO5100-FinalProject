83f40f7a5e1d7b4c3bb1c41e5c92d5fe1c568bbe
Ekoxier <hu.siz@northeastern.edu>
2021-11-24 00:05:09 -0500

    init

---
+++ EzShopping/src/Delivery/Transportation/Driver.java
@@ -0,0 +1,13 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Delivery.Transportation;
+
+/**
+ *
+ * @author Ekoxier
+ */
+public class Driver {
+    
+}
