a0b3913deef9984151dcee8949b535dd9b23cffc
Ekoxier <hu.siz@northeastern.edu>
2021-12-12 00:16:26 -0500

    modified

--- INFO5100-Final-Code/src/UI/WarehouseManagerWorkArea/InStockOptionPanel.java
+++ INFO5100-Final-Code/src/UI/WarehouseManagerWorkArea/InStockOptionPanel.java
@@ -115,6 +115,7 @@ public class InStockOptionPanel extends javax.swing.JPanel {
             int quantity = Integer.parseInt(this.jTextField1.getText());
             ProductOrg selectedProductOrg = (ProductOrg) this.jComboBox1.getSelectedItem();
             selectedProductOrg.addReuqestHandleList(this.saleMenuItem, quantity, this.warehouseOrg);
+            // 
             JOptionPane.showMessageDialog(this, "SUCCESS", "Result", -1);
         } catch (Exception e) {
             JOptionPane.showMessageDialog(
