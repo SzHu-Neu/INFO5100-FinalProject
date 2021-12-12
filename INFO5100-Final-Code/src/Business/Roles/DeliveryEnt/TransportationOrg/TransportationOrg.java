ffb00a38d3eca854819493d05a1040b0658accc5
Ekoxier <hu.siz@northeastern.edu>
2021-12-12 10:29:09 -0500

    merge

--- INFO5100-Final-Code/src/Business/Roles/DeliveryEnt/TransportationOrg/TransportationOrg.java
+++ INFO5100-Final-Code/src/Business/Roles/DeliveryEnt/TransportationOrg/TransportationOrg.java
@@ -2,46 +2,47 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
  */
-
 package Business.Roles.DeliveryEnt.TransportationOrg;
+
 import Business.Roles.DeliveryEnt.TransportationOrg.Driver;
 import java.util.List;
 
 import Business.Roles.DeliveryEnt.TransportationOrg.TransportationManager;
 import Business.Roles.DeliveryEnt.TransportationOrg.Driver;
 import java.util.List;
+
 /**
  *
  * @author Dom
  */
 public class TransportationOrg {
-       private String name;
-       private List<Driver> driverList;
-       private List<TransportationManager> transManagerList;
-       
-       public TransportationOrg(String name){
-            this.name = name;
-        }
-        
-       public Driver createDriver(){
-            Driver driver = new Driver(this);
-            this.driverList.add(driver);
-            return driver;
-        }
-       public void addDriver(Driver driver){
-            this.driverList.add(driver);
-        }
-       
-       public TransportationManager createTransportationManager(){
-            TransportationManager tm = new TransportationManager(this);
-            this.transManagerList.add(tm);
-            return tm;
-        }
-       public void addManager(TransportationManager tm){
-            this.transManagerList.add(tm);
-        }
 
-    private List<TransportationManager> managerList;
+    private String name;
     private List<Driver> driverList;
-    
+    private List<TransportationManager> transManagerList;
+
+    public TransportationOrg(String name) {
+        this.name = name;
+    }
+
+    public Driver createDriver() {
+        Driver driver = new Driver(this);
+        this.driverList.add(driver);
+        return driver;
+    }
+
+    public void addDriver(Driver driver) {
+        this.driverList.add(driver);
+    }
+
+    public TransportationManager createTransportationManager() {
+        TransportationManager tm = new TransportationManager(this);
+        this.transManagerList.add(tm);
+        return tm;
+    }
+
+    public void addManager(TransportationManager tm) {
+        this.transManagerList.add(tm);
+    }
+
 }
